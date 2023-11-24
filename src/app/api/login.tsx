f6757698a2306cc9f0a858ed5ecb3cfa3ac21bce
404kacper <kacper.sionkowski@gmail.com>
2023-11-24 13:46:59 +0100

    More directory structure refactoring to fit new next.js 13 conventions

--- src/app/api/login.tsx
+++ src/app/api/login.tsx
@@ -1,5 +1,6 @@
 import { API_URL } from '@/config/index';
 import { NextApiRequest, NextApiResponse } from 'next';
+import cookie from 'cookie';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
@@ -19,7 +20,17 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     const data = await strapiRes.json();
 
     if (strapiRes.ok) {
-      // @todo - Set cookie
+      res.setHeader(
+        'Set-Cookie',
+        cookie.serialize('token', data.jwt, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV !== 'development',
+          maxAge: 60 * 60 * 24 * 7, // 1 week
+          sameSite: 'strict',
+          path: '/',
+        })
+      );
+
       res.status(200).json({ user: data.user });
     } else {
       res
