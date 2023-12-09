d82b2fc2b059c39ea0721d50bf7353fc4eb54550
404kacper <kacper.sionkowski@gmail.com>
2023-12-08 23:32:30 -0800

    Started working on final component for dashobard page

---
+++ src/app/components/dashboard/files/downloads/Downloads.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './Indicators.module.scss';
+
+export default function Downloads() {
+  return <div className={styles.downloadsContainer}></div>;
+}
---
+++ src/app/components/dashboard/files/uploads/Uploads.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './Uploads.module.scss';
+
+export default function Uploads() {
+  return <div className={styles.uploadsContainer}></div>;
+}
